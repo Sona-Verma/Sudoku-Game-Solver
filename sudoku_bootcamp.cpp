3ef12318e560cc0630b96b8f10bdbafdb46dc839
Sona-Verma <77328436+Sona-Verma@users.noreply.github.com>
2021-01-12 16:36:26 +0530

    Update sudoku_bootcamp.cpp

--- sudoku_bootcamp.cpp
+++ sudoku_bootcamp.cpp
@@ -120,15 +120,6 @@ bool solveSuduko(int grid[N][N], int row, int col)
 int main()
 {
     // 0 means unassigned cells
-    /*int grid[N][N] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
-                       { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
-                       { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
-                       { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
-                       { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
-                       { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
-                       { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
-                       { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
-                       { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };*/
                        int grid[N][N];
                        cout<<"enter 9 x 9 Grid(Use 0 for empty)"<<endl;
                        for(int i=0;i<9;i++){
